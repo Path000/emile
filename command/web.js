b1c7a98a85fa7ef5c2a0953e4306746fd21741b1
Path000 <arrache.al@gmail.com>
2017-03-18 13:34:27 +0100

    class WebServer ES2015

--- command/web.js
+++ command/web.js
@@ -5,32 +5,34 @@ var socketio = require('socket.io');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
-const WebServer = (port) => {
-	EventEmitter.call(this);
-  var app = express();
-  var server = http.Server(app);
-  this.sio = socketio(server);
+class WebServer extends EventEmitter {
 
-  var self = this;
-  this.sio.on('connection', function (socket) {
-    self.emit('ready', {});
-    socket.on('command', function(data) {
-      self.receive(data);
+  constructor(port) {
+    var app = express();
+    var server = http.Server(app);
+    this.sio = socketio(server);
+
+    var self = this;
+    this.sio.on('connection', function (socket) {
+      self.emit('ready', {});
+      socket.on('command', function(data) {
+        self.receive(data);
+      });
     });
-  });
 
-  app.use(express.static(__dirname + '/static'));
+    app.use(express.static(__dirname + '/static'));
 
-  server.listen(port);
+    server.listen(port);
+  }
+
+  receive(data) {
+    this.emit(data.action, data.data);
+  }
+
+  send(action, data) {
+    this.sio.sockets.emit(action, data);
+  }
 }
-util.inherits(WebServer, EventEmitter);
 
-WebServer.prototype.receive = function (data) {
-	this.emit(data.action, data.data);
-}
-
-WebServer.prototype.send = function (action, data) {
-	this.sio.sockets.emit(action, data);
-}
 
 module.exports = WebServer;
