1a1b18e0908a5f45e45a78febb82c573c26cd874
Path000 <arrache.al@gmail.com>
2017-04-02 15:01:42 +0200

    fix

--- Robot.cpp
+++ Robot.cpp
@@ -43,7 +43,7 @@ void Robot::init() {
 }
 
 void Robot::directMove(byte servo, long pos) {
-	pos = constrain(pos, servos[servo]._min, servos[servo]._max);
+	//pos = constrain(pos, servos[servo]._min, servos[servo]._max);
 	_servoDriver.setPWM(servo, 0, pos);
 }
 
