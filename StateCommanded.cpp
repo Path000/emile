5e5d78fd90ec22e674e1077e5db9d1b8e07f896f
Path000 <arrache.al@gmail.com>
2017-04-02 14:55:36 +0200

    fix

--- StateCommanded.cpp
+++ StateCommanded.cpp
@@ -18,13 +18,13 @@ State *StateCommanded::run() {
 	}
 	if (data.cmd == COMMAND_SERVO) {
 		byte index = data.arrayArgs[0].toInt();
-		uint8_t pos = data.arrayArgs[1].toInt();
+		long pos = data.arrayArgs[1].toInt();
 		char *name = _robot->getServoName(index);
 		_robot->_ecran.showCommand(name, pos);
 			Serial.print("CONSOLE:");
 			Serial.print(name);
 			Serial.print(",");
-			Serial.println(data.arrayArgs[1]);
+			Serial.println(pos);
 
 
 		_robot->directMove(index, pos);
