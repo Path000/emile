b2d7ad583bebb4fdcfd16bb9e36be12290455556
Path000 <arrache.al@gmail.com>
2017-04-08 19:32:37 +0200

    inverse servo order after wiring 16 servo driver

--- Robot.cpp
+++ Robot.cpp
@@ -11,19 +11,19 @@ void Robot::init() {
 
 // relevé des min max avec le driver 16 servos
 
-	MyServo servoPD  ((char *)"Pied droit",      141, 300, 486); // 0
-	MyServo servoGD  ((char *)"Genou droit",     95, 95, 270); // 1
-	MyServo servoHD  ((char *)"Hanche droite",   296, 296, 464); // 2
-	MyServo servoCD  ((char *)"Coude droit",     117, 315, 315); // 3
-	MyServo servoEDX ((char *)"Epaule droite X", 114, 114, 489); // 4
-	MyServo servoEDZ ((char *)"Epaule droite Z", 125, 190, 460); // 5
-	MyServo servoT   ((char *)"Tete",            238, 369, 500); // 6
-	MyServo servoEGZ ((char *)"Epaule gauche Z", 115, 400, 470); // 7
-	MyServo servoEGX ((char *)"Epaule gauche X", 117, 320, 320); // 8
-	MyServo servoCG  ((char *)"Coude gauche",    279, 279, 430); // 9
-	MyServo servoHG  ((char *)"Hanche gauche",   227, 372, 372); // 10
-	MyServo servoGG  ((char *)"Genou gauche",    224, 414, 414); // 11
-	MyServo servoPG  ((char *)"Pied gauche",     195, 390, 510); // 12
+	MyServo servoPD  ((char *)"Pied droit",      141, 300, 486);
+	MyServo servoGD  ((char *)"Genou droit",     95, 95, 270);
+	MyServo servoHD  ((char *)"Hanche droite",   296, 296, 464);
+	MyServo servoCD  ((char *)"Coude droit",     117, 315, 315);
+	MyServo servoEDX ((char *)"Epaule droite X", 114, 114, 489);
+	MyServo servoEDZ ((char *)"Epaule droite Z", 125, 190, 460);
+	MyServo servoT   ((char *)"Tete",            238, 369, 500);
+	MyServo servoEGZ ((char *)"Epaule gauche Z", 115, 400, 470);
+	MyServo servoEGX ((char *)"Epaule gauche X", 117, 320, 320);
+	MyServo servoCG  ((char *)"Coude gauche",    279, 279, 430);
+	MyServo servoHG  ((char *)"Hanche gauche",   227, 372, 372);
+	MyServo servoGG  ((char *)"Genou gauche",    224, 414, 414);
+	MyServo servoPG  ((char *)"Pied gauche",     195, 390, 510);
 
 	servos[SERVO_PIED_D]     = servoPD;
 	servos[SERVO_GENOU_D]    = servoGD;
