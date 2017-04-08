527043a32868f75a0018db8bb0de24430d37a771
Path000 <arrache.al@gmail.com>
2017-04-08 19:21:47 +0200

    inverse servo order after wiring 16 servo driver

--- Robot.h
+++ Robot.h
@@ -9,19 +9,19 @@
 #include "MyServo.h"
 
 // Index in array
-#define SERVO_PIED_D      0
-#define SERVO_GENOU_D     1
-#define SERVO_HANCHE_D    2
-#define SERVO_COUDE_D     3
-#define SERVO_EPAULE_X_D  4
-#define SERVO_EPAULE_Z_D  5
+#define SERVO_PIED_D      12
+#define SERVO_GENOU_D     11
+#define SERVO_HANCHE_D    10
+#define SERVO_COUDE_D     9
+#define SERVO_EPAULE_X_D  8
+#define SERVO_EPAULE_Z_D  7
 #define SERVO_TETE        6
-#define SERVO_EPAULE_Z_G  7
-#define SERVO_EPAULE_X_G  8
-#define SERVO_COUDE_G     9
-#define SERVO_HANCHE_G    10
-#define SERVO_GENOU_G     11
-#define SERVO_PIED_G      12
+#define SERVO_EPAULE_Z_G  5
+#define SERVO_EPAULE_X_G  4
+#define SERVO_COUDE_G     3
+#define SERVO_HANCHE_G    2
+#define SERVO_GENOU_G     1
+#define SERVO_PIED_G      0
 
 #define NOMBRE_SERVO 13
 
